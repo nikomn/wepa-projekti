32584f62563ea5ef989aa38cac4b03189e501b81
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-11 16:28:57 +0300

    Projektin uudelleenaloitus

--- src/main/java/projekti/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package projekti;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
-}
